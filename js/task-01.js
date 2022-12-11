2b1fb85658f5146e90f03b82dcc01398b29fbd90
dashevskydmytro@gmail.com <dashevskydmytro@gmail.com>
2022-12-11 14:03:33 +0200

    first task done

---
+++ js/task-01.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const list = document.querySelector('#categories');
+
+const categories = list.children;
+console.log('Number of categories:', categories.length);
+
+const firstItem = list.firstElementChild;
+
+const secondItem = firstItem.nextElementSibling;
+
+const thirdItem = list.lastElementChild;
+
+
+
+console.log("Category:",firstItem.querySelector('h2').textContent);
+
+const firstItemList = firstItem.querySelector('ul')
+
+console.log("Elements:", firstItemList.children.length);
+
+
+
+console.log("Category:",secondItem.querySelector('h2').textContent);
+
+const secondItemList = secondItem.querySelector('ul');
+
+console.log("Elements:", secondItemList.children.length);
+
+
+
+console.log("Category:",thirdItem.querySelector('h2').textContent);
+
+const thirdItemList = thirdItem.querySelector('ul');
+
+console.log("Elements:", thirdItemList.children.length);

0d0e9e3146ee54180c6dd78d21b550b61a1b5a88
dashevskydmytro@gmail.com <dashevskydmytro@gmail.com>
2022-12-11 14:29:30 +0200

    second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+
+for (const ingredient of ingredients) {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  ingredientsList.append(item)
+}
+
+
+
+
+/* const markup = ingredients
+  .map((item) => `<li class="list-item">${item}</li>`)
+  .join("");
+
+
+console.log(markup);
+
+
+ingredientsList.innerHTML = markup; */

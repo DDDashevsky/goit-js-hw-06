341ed96f635f481a03eab26bbb6ff635a717c8bf
dashevskydmytro@gmail.com <dashevskydmytro@gmail.com>
2022-12-11 19:50:55 +0200

    task-1 update

--- js/task-01.js
+++ js/task-01.js
@@ -1,36 +1,31 @@
-'use strict'
-
 const list = document.querySelector('#categories');
 
 const categories = list.children;
 console.log('Number of categories:', categories.length);
 
-const firstItem = list.firstElementChild;
+for (const category of categories) {
+  console.log(category.querySelector('h2').textContent);
+  console.log(category.querySelector('ul').children.length);
+}
 
+/* const firstItem = list.firstElementChild;
 const secondItem = firstItem.nextElementSibling;
-
 const thirdItem = list.lastElementChild;
 
 
 
 console.log("Category:",firstItem.querySelector('h2').textContent);
-
 const firstItemList = firstItem.querySelector('ul')
-
 console.log("Elements:", firstItemList.children.length);
 
 
 
 console.log("Category:",secondItem.querySelector('h2').textContent);
-
 const secondItemList = secondItem.querySelector('ul');
-
 console.log("Elements:", secondItemList.children.length);
 
 
 
 console.log("Category:",thirdItem.querySelector('h2').textContent);
-
 const thirdItemList = thirdItem.querySelector('ul');
-
-console.log("Elements:", thirdItemList.children.length);
+console.log("Elements:", thirdItemList.children.length); */

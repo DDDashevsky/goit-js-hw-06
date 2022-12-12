3222c07ae56b615b043381f29b676c6b420c228e
dashevskydmytro@gmail.com <dashevskydmytro@gmail.com>
2022-12-13 00:04:37 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const button = document.querySelector('.change-color');
+const hexOutput = document.querySelector('.color');
+
+button.addEventListener('click', changeColor);
+
+function changeColor(evt) {
+  hexOutput.textContent = String(getRandomHexColor());
+  document.querySelector('body').style.backgroundColor = hexOutput.textContent;
+}

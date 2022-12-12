f0d6c4313ccaaca13b54195bfbdac5784601d724
dashevskydmytro@gmail.com <dashevskydmytro@gmail.com>
2022-12-12 23:54:05 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', validationCheck);
+function validationCheck(evt) {
+  evt.preventDefault();
+  const { email, password } = evt.currentTarget.elements;
+  if (email.value === '' || password.value === '') {
+    alert('Please enter yor email and password');
+  } else {
+    const user = new Object();
+    user.Email = email.value;
+    user.Password = password.value;
+    console.log(user);
+    evt.currentTarget.reset();
+  }
+}
